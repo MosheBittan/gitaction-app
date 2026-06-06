971a288b147da37c7d8774d609c08287e9c84e5a
MosheBittan <moshebitgit@gmail.com>
2026-06-06 17:10:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+def main():
+    print(f"Python version: {sys.version}")
+    print("Application successfully initialized inside the Docker container.")
+    print("Pipeline integration test: SUCCESS.")
+
+if __name__ == "__main__":
+    main()
